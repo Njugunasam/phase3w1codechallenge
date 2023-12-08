fa724983657ff3691f114922e8c7aaaafe28b8e7
Njugunasam <sammynjuguna5826@gmail.com>
2023-12-08 17:18:06 +0300

    time converting

---
+++ lib/converting_time.py
@@ -0,0 +1,14 @@
+# function for converting time
+def convert_12hrs_to_24hrs(hour, minute, period):
+    # conditinal statement
+
+    if period == "am" and hour == 12:
+        hour = 0
+    elif period == "pm" and hour != 12:
+        hour += 12
+
+    return f"{hour:02d}{minute:02d}"
+
+
+result = convert_12hrs_to_24hrs(12, 30, "am")
+print(result)
